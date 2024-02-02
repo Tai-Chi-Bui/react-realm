98184d49fe0d57861dc42781cd9af8838cf10bdf
Tai Bui <tai.c.bui@gmail.com>
2024-02-02 11:55:25 +0700

    refresh

--- src/status/status/offline.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/status.module.css'
-
-interface Props {}
-
-const Offline = () => {
-  return <div className={`${styles.statusElement} ${styles.offline}`}></div>
-}
-
-export default Offline
--- src/utils/generateRandomString.ts
+++
@@ -1,5 +0,0 @@
-function generateRandomString(prefix: string): string {
-  return `${prefix}-${Math.random().toString(36).substring(2)}`
-}
-
-export default generateRandomString
